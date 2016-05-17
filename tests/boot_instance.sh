a05c6384afc3ec73e02aa92370a26f24de917356
Michael Solberg <msolberg@redhat.com>
2016-05-17 10:31:49 -0400

    Allow overriding the instance parameters. Only take the first floating IP.

--- tests/boot_instance.sh
+++ tests/boot_instance.sh
@@ -3,6 +3,16 @@
 # Get credentials
 . ~/.openstackrc
 
+# Get instance config
+if [ -f ~/.instancerc ]
+then
+  . ~/.instancerc
+else
+  export FLAVOR=m1.small
+  export IMAGE=CentOS-7.1-x86_64-GenericCloud-1503
+  export KEY_NAME=jenkins
+fi
+
 # Find a floating IP for our app.
 # Either reuse the one we have or get a free one.
 for SERVER in `/usr/bin/nova list | grep jenkins-${JOB_NAME} | awk '{print $2}'`
@@ -23,11 +33,14 @@ then
   /usr/bin/nova stop $FLOATING_IP_HOST
 else
   # Else, let's find a free one
-  FLOATING_IP=`/usr/bin/nova floating-ip-list | grep ' - ' | awk '{print $4}'`
+  FLOATING_IP=`/usr/bin/nova floating-ip-list | grep ' - ' | awk '{print $4}' | head -1`
 fi
 
 # Build our instance
-/usr/bin/nova boot --flavor m1.small --image CentOS-7.1-x86_64-GenericCloud-1503 --user-data user_data.txt $BUILD_TAG
+/usr/bin/nova boot --flavor ${FLAVOR} \
+  --image ${IMAGE} \
+  --key-name ${KEY_NAME} \
+  --user-data user_data.txt $BUILD_TAG
 
 # Associate a free floating IP with the instance
 if [ $FLOATING_IP ]
